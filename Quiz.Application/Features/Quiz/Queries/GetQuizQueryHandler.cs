231ac244730ca242995a3caa76e1be88b12eec4a
Lionel <lionelhuizing@gmail.com>
2026-03-17 10:01:16 +0100

    Added QuizTemplateId to QuestionDto

--- Quiz.Application/Features/Quiz/Queries/GetQuizQueryHandler.cs
+++ Quiz.Application/Features/Quiz/Queries/GetQuizQueryHandler.cs
@@ -21,6 +21,7 @@ namespace Quiz.Application.Features.Quiz.Queries
             {
                 Id = q.Id,
                 QuestionText = q.QuestionText,
+                QuestionTemplateTypeId = q.QuizTemplateId,
                 MediaType = q.MediaType.ToString(),
                 MediaUrl = q.MediaUrl,
                 Answers = q.Answers.Select(a => new AnswerDto
